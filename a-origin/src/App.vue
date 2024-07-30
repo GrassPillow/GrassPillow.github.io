015f31bd6866e791f14755e98969007317c6d5ea
GrassPillow <34212900+GrassPillow@users.noreply.github.com>
2024-07-30 23:52:28 +0800

    commit

---
+++ a-origin/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+    <digital-clock id="digital-clock-1"></digital-clock>
+  </div>
+</template>
+
+<script>
+import DigitalClock from "./components/DigitalClock.vue";
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: "App",
+  components: {
+    DigitalClock,
+    // HelloWorld
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: rgb(102, 126, 126);
+  margin: 0;
+  padding: 0;
+}
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50; */
+  /* margin-top: 60px; */
+}
+
+#digital-clock-1 {
+  height: 100vh;
+  font-size: 16vw;
+  line-height: 100vh;
+  font-family: "Courier New", Courier, monospace;
+}
+</style>
---
+++ a-origin/src/components/DigitalClock.vue
@@ -0,0 +1,46 @@
+<template>  
+    <div class="digital-clock">  
+      {{ formattedTime }}  
+    </div>  
+  </template>  
+    
+  <script>  
+  import { ref, onMounted, onUnmounted } from 'vue';  
+    
+  export default {  
+    name: 'DigitalClock',  
+    
+    setup() {  
+      const currentTime = ref(new Date());  
+      const formattedTime = ref('');  
+    
+      const updateTime = () => {  
+        currentTime.value = new Date();  
+        formattedTime.value = currentTime.value.toLocaleTimeString();  
+      };  
+    
+      const timerId = setInterval(updateTime, 1000);  
+    
+      onMounted(() => {  
+        updateTime();  
+      });  
+    
+      onUnmounted(() => {  
+        clearInterval(timerId);  
+      });  
+    
+      return {  
+        formattedTime,  
+      };  
+    },  
+  };  
+  </script>  
+    
+  <style scoped>  
+  .digital-clock {  
+    text-align: center;  
+    /* font-size: 36dvh;  
+    padding-top: 32dvh; */
+    font-family: 'Arial', sans-serif;  
+  }  
+  </style>
